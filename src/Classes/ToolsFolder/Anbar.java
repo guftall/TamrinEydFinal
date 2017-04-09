7eaa79a42f5338f64bfc9f0a76e98ab5f2000dcc
Omid Dehghani <guftall@chmail.ir>
2017-04-09 11:55:26 +0430

    Add Anbar Class
    اضافه کردن کلاس انبار برای استفاده در قلعه
    هر خاندان میتواند تعداد را بگیرد و به کالا های در انبار قلعه ی خود اضافه
    کند

---
+++ src/Classes/ToolsFolder/Anbar.java
@@ -0,0 +1,14 @@
+package Classes.ToolsFolder;
+
+public class Anbar {
+
+	private int tedadeKala;
+
+	public int getTedadeKala() {
+		return tedadeKala;
+	}
+
+	public void setTedadeKala(int tedadeKala) {
+		this.tedadeKala = tedadeKala;
+	}
+}
