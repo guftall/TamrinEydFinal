6e9be3907c16598f2094fdd3ec5c9deca36d6e7a
Omid Dehghani <guftall@chmail.ir>
2017-04-09 14:42:38 +0430

    Add Heyvans Detail
    تغییر دادن کلاس های حیوانی

--- src/Classes/ToolsFolder/Ejdeha.java
+++ src/Classes/ToolsFolder/Ejdeha.java
@@ -2,19 +2,21 @@ package Classes.ToolsFolder;
 
 public class Ejdeha extends Heyvan {
 
-	public int power;
-	
 	public void tokhmGozari() {
 		
 	}
 	
-	public void partabeAtash() {
-		
-	}
 
 	@Override
 	public void move() {
 		// TODO Auto-generated method stub
 		// Parvaz Kardan
+	}
+
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		// Partabe Atash
 	}
 }
--- src/Classes/ToolsFolder/Gorg.java
+++ src/Classes/ToolsFolder/Gorg.java
@@ -2,23 +2,20 @@ package Classes.ToolsFolder;
 
 public class Gorg extends Heyvan {
 
-	public int ghodrat;
-	
-	public void gazGereftan() {
-		
-	}
 
 	public void ghoRRRRResh() {
 		
 	}
 	
-	public void Davidan() {
-		
-	}
-
 	@Override
 	public void move() {
 		// TODO Auto-generated method stub
 		// Davidan
+	}
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		// Gaz gereftan
 	}
 }
