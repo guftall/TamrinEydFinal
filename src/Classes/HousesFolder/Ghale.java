3f4cd19c6a28dd83d31b8a5c730695d6c288b432
Omid Dehghani <guftall@chmail.ir>
2017-04-09 12:10:51 +0430

    Add Anbar To Ghale
    اضافه کردن فیلد انبار به قلعه

--- src/Classes/HousesFolder/Ghale.java
+++ src/Classes/HousesFolder/Ghale.java
@@ -1,10 +1,14 @@
 package Classes.HousesFolder;
 
+import Classes.ToolsFolder.Anbar;
 import Classes.ToolsFolder.SarbazKhane;
 
 public class Ghale {
 	
 	private SarbazKhane sarbazKhane;
+	private Anbar anbar;
+	
+	
 
 	public SarbazKhane getSarbazKhane() {
 		return sarbazKhane;
@@ -12,6 +16,14 @@ public class Ghale {
 
 	public void setSarbazKhane(SarbazKhane sarbazKhane) {
 		this.sarbazKhane = sarbazKhane;
+	}
+
+	public Anbar getAnbar() {
+		return anbar;
+	}
+
+	public void setAnbar(Anbar anbar) {
+		this.anbar = anbar;
 	}
 
 }
