44189331eb39b06da458ad409d7434b99d990aff
Omid Dehghani <guftall@chmail.ir>
2017-04-09 14:23:30 +0430

    Add Ghale Details
    اضافه کردن مشخصات به قلعه

--- src/Classes/HousesFolder/MirinGhale.java
+++ src/Classes/HousesFolder/MirinGhale.java
@@ -1,5 +1,11 @@
 package Classes.HousesFolder;
 
+import Classes.ToolsFolder.Tools.Khandanha;
+
 public class MirinGhale extends Ghale {
+	
+	public MirinGhale() {
+		super("Herame Bozorge Mirin", "Esus", Khandanha.Targrin);
+	}
 
 }
--- src/Classes/HousesFolder/WinterFellGhale.java
+++ src/Classes/HousesFolder/WinterFellGhale.java
@@ -1,5 +1,11 @@
 package Classes.HousesFolder;
 
+import Classes.ToolsFolder.Tools.Khandanha;
+
 public class WinterFellGhale extends Ghale {
+	
+	public WinterFellGhale() {
+		super("Winter Fell", "Shomale Vestrus", Khandanha.Stark);
+	}
 
 }
