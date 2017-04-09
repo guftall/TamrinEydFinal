30840dd332b4e5b02b96b92ddcb540361b6f449a
Omid Dehghani <guftall@chmail.ir>
2017-04-09 11:50:50 +0430

    Add SarbazKhane To Ghale
    اضافه کردن فیلد سربازخانه به هر قلعه

--- src/Classes/HousesFolder/Ghale.java
+++ src/Classes/HousesFolder/Ghale.java
@@ -1,5 +1,17 @@
 package Classes.HousesFolder;
 
+import Classes.ToolsFolder.SarbazKhane;
+
 public class Ghale {
+	
+	private SarbazKhane sarbazKhane;
+
+	public SarbazKhane getSarbazKhane() {
+		return sarbazKhane;
+	}
+
+	public void setSarbazKhane(SarbazKhane sarbazKhane) {
+		this.sarbazKhane = sarbazKhane;
+	}
 
 }
