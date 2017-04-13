3ea13efc0e9b75a1d82ee02f8b6a1651bd2f8e0b
Omid Dehghani <guftall@chmail.ir>
2017-04-13 15:00:54 +0430

    Some Change - SarbazKhand
    اضافه کردن متد به سربازخانه
    که بهخ تعداد تصادفی سرباز تولید میکنه
    و اضافه کردن کد به کانستراکتور سربازخانه که با ایجاد شدنش یه تعداد
    تصادفی سرباز به سربازخانه اضافه کنه.
    اینجوری هر سربازخانه هنگام ساخته شدن یه تعداد سرباز داره

--- src/Classes/ToolsFolder/SarbazKhane.java
+++ src/Classes/ToolsFolder/SarbazKhane.java
@@ -1,15 +1,27 @@
 package Classes.ToolsFolder;
 
+import java.util.Random;
+
 public class SarbazKhane {
 	
 	private int sarbazHa;
 	
+	public SarbazKhane() {
+		addSarbaz();
+	}
+	
 	public void addSarbaz(int number) {
-		
+		sarbazHa += number;
+	}
+	
+	public void addSarbaz() {
+		Random random = new Random();
+		sarbazHa += random.nextInt(7000);
 	}
 	
 	public int getSarbazHaNumber() {
 		return sarbazHa;
 	}
+	
 
 }
